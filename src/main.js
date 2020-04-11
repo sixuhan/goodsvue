21c747a1a2ddd37e50c92fc698ed38a170ca54ff
zhangtianxu <a823525514@gmail.com>
2020-04-11 12:44:52 +0800

    0.1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import VueRouter from 'vue-router';
+
+
+Vue.use(Vant);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
